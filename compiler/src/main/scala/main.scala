40ac7a2ee3eaa5ff026e3bca720fa47628c1041e
Arthur Peters <amp@singingwizard.org>
2025-12-14 14:41:07 -0600

    Add Scala compiler project

---
+++ compiler/src/main/scala/main.scala
@@ -0,0 +1,9 @@
+import langjam.parser.Node.Var
+import langjam.parser.NodeParser
+
+import scala.util.parsing.combinator.*
+import scala.util.parsing.input.{CharSequenceReader, Reader}
+
+@main
+def main(): Unit =
+  println("Hello")
